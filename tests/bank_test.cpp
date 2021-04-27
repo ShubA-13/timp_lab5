369688c0e1dbefbf45c4e2ea2269f07f4307834b
ShubA-13 <yura.shubnyj@mail.ru>
2021-04-27 22:49:36 +0300

    try7

--- tests/bank_test.cpp
+++ tests/bank_test.cpp
@@ -13,11 +13,12 @@
    // MOCK_METHOD(int, GetBalance, (), (const, override));
    // MOCK_METHOD(void, ChangeBalance, (int diff), (override));
    // MOCK_METHOD(void, Lock, (), (override));
-
+   MOCK_METHOD(void, Unlock, (), (override));
+  };
 
   class MockTransaction: public Transaction{
-
-   
+  public:
+   MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
   };
 
   TEST(Account, GetBalance){
